2e34583aad17f545e6d96c26dcb040beefa5a0bf
FinnE145 <finn.emmerson@renertschool.ca>
2024-12-11 17:26:43 -0700

    Server: Change secret and add to .env file

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '1ts4s3cr3t'
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
