b450079d6809a1ae0b95e6912f99d464c06a24b7
FinnE145 <finn.emmerson@renertschool.ca>
2024-12-15 09:42:20 -0700

    Server: Move updating matchups to a seperate script, add matchups for dec 15

--- app.py
+++ app.py
@@ -12,24 +12,6 @@ import pytz
 
 # dt.datetime.now(dt.timezone.utc)
 
-matchups = [
-    {"Home": "New Jersey Devils", "Away": "Chicago Blackhawks", "Datestr": "11:00 am"},
-    {"Home": "New York Rangers", "Away": "Los Angeles Kings", "Datestr": "11:00 am"},
-    {"Home": "Minnesota Wild", "Away": "Philadelphia Flyers", "Datestr": "12:00 pm"},
-    {"Home": "Edmonton Oilers", "Away": "Vegas Golden Knights", "Datestr": "2:00 pm"},
-    {"Home": "Ottawa Senators", "Away": "Pittsburgh Penguins", "Datestr": "5:00 pm"},
-    {"Home": "Detroit Red Wings", "Away": "Toronto Maple Leafs", "Datestr": "5:00 pm"},
-    {"Home": "Washington Capitals", "Away": "Buffalo Sabres", "Datestr": "5:00 pm"},
-    {"Home": "Columbus Blue Jackets", "Away": "Anaheim Ducks", "Datestr": "5:00 pm"},
-    {"Home": "Winnipeg Jets", "Away": "Montreal Canadiens", "Datestr": "5:00 pm"},
-    {"Home": "Dallas Stars", "Away": "St. Louis Blues", "Datestr": "6:00 pm"},
-    {"Home": "Colorado Avalanche", "Away": "Nashville Predators", "Datestr": "7:00 pm"},
-    {"Home": "Calgary Flames", "Away": "Florida Panthers", "Datestr": "8:00 pm"},
-    {"Home": "Vancouver Canucks", "Away": "Boston Bruins", "Datestr": "8:00 pm"},
-    {"Home": "San Jose Sharks", "Away": "Utah Hockey Club", "Datestr": "8:00 pm"},
-    {"Home": "Seattle Kraken", "Away": "Tampa Bay Lightning", "Datestr": "8:00 pm"}
-]
-
 app = Flask(__name__)
 
 load_dotenv()
@@ -388,32 +370,5 @@ def logout():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    with app.app_context():
-        if input("Reset matchups database? ").lower() in ["y", "yes"]:
-            db.session.query(Matchup).delete()
-            db.session.commit()
-
-        if input("Add matchups? ").lower() in ["y", "yes"]:
-            for matchup in matchups:
-                new_matchup = Matchup(Datetime=dt.datetime.strptime("2024-12-14 " + matchup["Datestr"], "%Y-%m-%d %I:%M %p"), Home=matchup["Home"], Away=matchup["Away"])
-                db.session.add(new_matchup)
-                db.session.commit()
-
-            """ new_matchup = Matchup(Datetime=dt.datetime.strptime("2024-12-13 11:00 am", "%Y-%m-%d %I:%M %p"), Home="Old Test", Away="Old Test")
-            db.session.add(new_matchup)
-            db.session.commit() """
-
     app.run(debug=True)
-else:
-    with app.app_context():
-        db.session.query(Matchup).filter_by(MatchupId=2).delete()
-        db.session.commit()
-
-        mst = pytz.timezone("US/Mountain")
-
-        for matchup in matchups:
-            datetime = mst.localize(dt.datetime.strptime("2024-12-14 " + matchup["Datestr"], "%Y-%m-%d %I:%M %p"))
-            new_matchup = Matchup(Datetime=datetime.astimezone(dt.timezone.utc), Home=matchup["Home"], Away=matchup["Away"])
-            db.session.add(new_matchup)
-            db.session.commit()
     
---
+++ update_matchups.py
@@ -0,0 +1,23 @@
+from app import app, db, Matchup
+import datetime as dt
+import pytz
+
+matchups = [
+    {"Home": "Chicago Blackhawks", "Away": "New York Islanders", "Datestr": "1:00 pm"},
+    {"Home": "Toronto Maple Leafs", "Away": "Buffalo Sabres", "Datestr": "3:00 pm"},
+    {"Home": "Carolina Hurricanes", "Away": "Columbus Blue Jackets", "Datestr": "3:00 pm"},
+    {"Home": "New York Rangers", "Away": "St. Louis Blues", "Datestr": "4:00 pm"},
+    {"Home": "Minnesota Wild", "Away": "Vegas Golden Knights", "Datestr": "4:00 pm"}
+]
+
+with app.app_context():
+        db.session.query(Matchup).filter_by(MatchupId=2).delete()
+        db.session.commit()
+
+        mst = pytz.timezone("US/Mountain")
+
+        for matchup in matchups:
+            datetime = mst.localize(dt.datetime.strptime("2024-12-14 " + matchup["Datestr"], "%Y-%m-%d %I:%M %p"))
+            new_matchup = Matchup(Datetime=datetime.astimezone(dt.timezone.utc), Home=matchup["Home"], Away=matchup["Away"])
+            db.session.add(new_matchup)
+            db.session.commit()
