a954f4de7d3364b86bb667d108a064efd3e1f05d
FinnE145 <finn.emmerson.12@gmail.com>
2024-12-19 13:24:18 -0700

    Update Matchups: Dec 19

--- update_matchups.py
+++ update_matchups.py
@@ -3,11 +3,15 @@ import datetime as dt
 import pytz
 
 matchups = [
-    {"Away": "Philadelphia Flyers", "Home": "Detroit Redwings", "Datestr": "2024-12-17 5:00 pm"},
-    {"Away": "Toronto Maple Leafs", "Home": "Dallas Stars", "Datestr": "2024-12-17 5:30 pm"},
-    {"Away": "Florida Panthers", "Home": "Minnesota Wild", "Datestr": "2024-12-17 7:30 pm"},
-    {"Away": "Vancouver Canucks", "Home": "Utah Hockey Club", "Datestr": "2024-12-17 8:00 pm"},
-    {"Away": "Winnipeg Jets", "Home": "Anaheim Ducks", "Datestr": "2024-12-17 8:00 pm"}
+    {"Away": "St. Louis Blues", "Home": "Tampa Bay Lightning", "Datestr": "2024-12-19 5:00 pm"},
+    {"Away": "New Jersey Devils", "Home": "Columbus Blue Jackets", "Datestr": "2024-12-19 5:00 pm"},
+    {"Away": "Los Angeles Kings", "Home": "Philadelphia Flyers", "Datestr": "2024-12-19 5:30 pm"},
+    {"Away": "Pitsburg Penguins", "Home": "Nashville Predators", "Datestr": "2024-12-19 6:00 pm"},
+    {"Away": "Seattle Kraken", "Home": "Chicago Blackhawks", "Datestr": "2024-12-19 6:30 pm"},
+    {"Away": "Ottawa Sentators", "Home": "Calgary Flames", "Datestr": "2024-12-19 7:00 pm"},
+    {"Away": "Boston Bruins", "Home": "Edmonton Oilers", "Datestr": "2024-12-19 7:00 pm"},
+    {"Away": "Vancouver Canucks", "Home": "Vegas GOlden Knights", "Datestr": "2024-12-19 8:00 pm"},
+    {"Away": "Colorado Avalanche", "Home": "San Jose Sharks", "Datestr": "2024-12-19 8:30 pm"}
 ]
 
 with app.app_context():
